6988538784083b86db45142be43505fc208585c8
Your Name <you@example.com>
2019-04-09 15:01:42 +0530

    Add newMessage and createMessage events

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+var socket = io();
+
+socket.on('connect', function ()  {
+    console.log('Connected to server');
+
+    socket.emit('createMessage', {
+        from: 'ashok',
+        text: 'yup'
+    });
+})
+
+socket.on('disconnect', function () {
+    console.log('Disconnected from the server');
+});
+
+socket.on('newMessage', function(message) {
+ console.log('newMessage', message);
+});
